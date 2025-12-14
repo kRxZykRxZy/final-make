87cd737d22884b3daa9c6eb11c6582868e04107e
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-14 11:34:54 +0000

    Add firebase config creds

--- app/lib/firebase/firebaseConfig.js
+++ app/lib/firebase/firebaseConfig.js
@@ -1,3 +1,11 @@
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyC4qbNtBOiEByqS24lzE0GpTaj_4twePI8",
+  authDomain: "codebuild-4f90c.firebaseapp.com",
+  projectId: "codebuild-4f90c",
+  storageBucket: "codebuild-4f90c.firebasestorage.app",
+  messagingSenderId: "729840097532",
+  appId: "1:729840097532:web:3a74c15b638f1715365f79",
+  measurementId: "G-MJGX7TRL11"
+};
 
 export { firebaseConfig };
